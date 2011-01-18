218c35f3685d0e87b64aca349403632ffdfcf238
iwuerstchen <iWuerstchen@googlemail.com>
2011-01-18 19:14:21 +0000

    Extended command line argument parsing; implemented support for URLs, instead of IPs only; implemented help screen

--- checker.c
+++ checker.c
@@ -36,60 +36,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
-#ifndef _WIN32
-  #include <sys/socket.h>
-  #include <sys/types.h>
-  #include <sys/param.h>
-  #include <netinet/in.h>
-  #include <unistd.h>
-  #include <signal.h>
-  #include <netinet/in.h>
-  #include <arpa/inet.h>
-  #include <netdb.h>
-#endif
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/param.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <signal.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
-#define HTTP_HEADER "HEAD %s HTTP/1.1\r\nHost: www.macnews.de\r\nConnection: close\r\n\r\n"
+#define FALSE 0
+#define TRUE 1
+#define HTTP_HEADER "HEAD %s HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n"
 
-#ifndef _WIN32
+void show_help() {
+  printf("Help:\n \
+    -h            Show this screen\n \
+    -u <url|ip>   URL or IP address to check\n \
+    -f <filename> File with URLs to check\n\n");      
+}
+
+
 int main(int argc, char **argv) {
   FILE *fd;
   FILE *out;
   char *buf, c, url[256], buffer[65536];
-  int i, sock, res, round, valid, invalid;
+  int i, sock, res, round, valid, invalid, o=-1;  
   struct sockaddr_in addr;
-  struct hostent *h;
+  struct hostent *h = NULL;
   char befehl[386] , message[256];
   
   /** 
    * Check command line arguments 
-   */
-  if( argc < 3 ) {
-    printf("usage: %s <ip address> <filename>\n", argv[0]);
-    return 1;
-  }
-    
-  /** 
-   * Load file with URLs 
-   */
-  fd = fopen(argv[2], "r");
-  if( fd == NULL ) {
-    fprintf(stderr, "Error: Cannot open file!\n");
-    return 1;
-  }
-  
-  /**
-   * Check IP address
-   * TODO Implement DNS fallback
-   */
-  if( inet_addr(argv[1]) == INADDR_NONE ) {
-    fprintf(stderr, "Error: Invalid IP address!\n");
-    return 1;
-  }
+   */  
+  while( (o = getopt(argc, argv, "hu:f:")) != -1 ) {
+    switch(o) {    
+      case 'h':
+        show_help();
+        return 0;
+        break;
+        
+      case 'u':
+        if( inet_addr(optarg) == INADDR_NONE && (h = gethostbyname(optarg)) == NULL ) {    
+          fprintf(stderr, "Error: Invalid address!\n"); 
+          return 1;
+        }        
+        if( h != NULL ) {
+          memcpy(&addr.sin_addr, h->h_addr_list[0], h->h_length);
+        } else {    
+          addr.sin_addr.s_addr = inet_addr(optarg);
+        }
+      break;
       
+      case 'f':
+        fd = fopen(optarg, "r");
+        if( fd == NULL ) {
+          fprintf(stderr, "Error: Cannot open file!\n");
+          return 1;
+        }
+      break;
+        
+      case '?':
+        if( optopt == 'u' || optopt == 'f' ) {
+          fprintf(stderr, "Error: Option -%c requires an argument!\n", optopt);
+          return 1;
+        } else {
+          fprintf(stderr, "Error: Unknown option -%c!\n", optopt);
+          return 1;
+        }
+        break;
+        
+      default:
+        printf("usage: %s <arguments...>\nTry \"%s -h\" for help!\n", argv[0], argv[0]);
+        return 1;
+        break;
+    }
+  }  
+          
   addr.sin_family = AF_INET;
   addr.sin_port = htons(80);  
-  addr.sin_addr.s_addr = inet_addr(argv[1]);
   valid = invalid = 0;
   round  = 1;
   while( c != EOF ) {    
@@ -104,29 +132,31 @@ int main(int argc, char **argv) {
     /** 
      * Read in one line 
      */
-    while( (c = getc(fd)) != '\n' ) {
+    while( (c = getc(fd)) != '\n' && c != EOF ) {
       url[i++] = c;
     }
+    if( strlen(url) <= 0 ) {
+      continue;
+    }
     url[i] = '\0';
-    sprintf(befehl, HTTP_HEADER, url);
+    sprintf(befehl, HTTP_HEADER, url, argv[1]);
     
     /* Connect to address and try to fetch HTTP header */
     if( connect(sock, (struct sockaddr*) &addr, sizeof(addr)) != -1 ) {      
       write(sock, befehl, sizeof(befehl));
-      res = read(sock, buffer, sizeof(buffer));            
+      res = read(sock, buffer, sizeof(buffer));          
       if( strstr(buffer, "HTTP/1.1 200") == NULL) {        
         if( strstr(buffer, "HTTP/1.1 301") != NULL) {
           sprintf(message, "301: %s\n\0", url);
         } else if( strstr(buffer, "HTTP/1.1 302") != NULL) {
           sprintf(message, "302: %s\n\0", url);
         } else if( strstr(buffer, "HTTP/1.1 404") != NULL) {
-          sprintf(message, "404: %s\n\0", url);
+          sprintf(message, "404: %s - (%d)\n\0", url, strlen(url));
         } else if( strstr(buffer, "HTTP/1.1 400") != NULL) {
           sprintf(message, "400: %s\n\0", url);
         } else {
           sprintf(message, "UNKNOWN: %s\n\0", url);
-        }
-        /*fprintf(stdout, "%s", message);*/
+        }        
         fwrite(message, sizeof(char), strlen(message), out);
       }
       
@@ -141,11 +171,7 @@ int main(int argc, char **argv) {
     fclose(out);
   } 
   fclose(fd);  
+  printf("All done! Terminating...\n");
   return 0;
 }
-#else
-  int main() {
-    fprintf("Windows systems are not supported, yet!\n");
-    return 1;
-  }
-#endif
+
